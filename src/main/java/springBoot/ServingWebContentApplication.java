a65db071ee3df0a30bf31ec17cb5ae79d6cdc5af
Vladimir <vtischenko@meta.ua>
2020-04-21 07:55:00 +0300

    init

---
+++ src/main/java/springBoot/ServingWebContentApplication.java
@@ -0,0 +1,14 @@
+package springBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Vladimir on 20.04.2020.
+ */
+@SpringBootApplication
+public class ServingWebContentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServingWebContentApplication.class, args);
+    }
+}
