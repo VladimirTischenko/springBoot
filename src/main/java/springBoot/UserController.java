5ac57d1374d50298dadc20dc34c8234371f68872
Vladimir <vtischenko@meta.ua>
2020-04-22 11:02:25 +0300

    make addNewUser() return User

--- src/main/java/springBoot/UserController.java
+++ src/main/java/springBoot/UserController.java
@@ -22,13 +22,11 @@ public class UserController {
     }
 
     @PostMapping // Map ONLY POST Requests
-    public @ResponseBody String addNewUser (@RequestParam String name, @RequestParam String email) {
-        // @ResponseBody means the returned String is the response, not a view name
+    public @ResponseBody User addNewUser (@RequestParam String name, @RequestParam String email) {
         // @RequestParam means it is a parameter from the GET or POST request
 
         User user = new User(name, email);
-        userRepository.save(user);
-        return "Saved";
+        return userRepository.save(user);
     }
 
     @GetMapping
